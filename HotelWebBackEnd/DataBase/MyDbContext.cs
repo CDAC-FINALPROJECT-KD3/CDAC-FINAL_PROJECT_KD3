888ceeeadc66e7f9e1869da80cfc1a5192823ba0
drn-sharma99 <drn.sharma99@gmail.com>
2025-02-06 00:59:43 +0530

    .NET BackEnd

---
+++ HotelWebBackEnd/DataBase/MyDbContext.cs
@@ -0,0 +1,25 @@
+﻿using HotelWebBackEnd.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelWebBackEnd.DataBase
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions options):base(options) { }
+
+        public DbSet<Emp> emps { get; set; }
+        public DbSet<User> users { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<User>(entity =>
+            {
+                //Set default value for isActive in the database
+                entity.Property(e => e.IsActive)
+                      .HasDefaultValue(true);
+            });
+        }
+    }
+
+}
