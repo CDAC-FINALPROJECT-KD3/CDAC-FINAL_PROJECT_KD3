01d4966ba2657edd7ce59167e85cef71bd9cdd9d
Yagywalky <yagywalkydubey08@gmail.com>
2025-02-09 10:31:55 +0530

    User Backend

---
+++ Backend/hotelbooking/src/main/java/com/hotelbooking/exception/MyException.java
@@ -0,0 +1,10 @@
+package com.hotelbooking.exception;
+
+public class MyException extends RuntimeException{
+	
+	public MyException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+}
