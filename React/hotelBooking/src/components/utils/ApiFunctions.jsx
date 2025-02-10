1873b937261e7c0eba29490c78c4774d1276c895
Yagywalky <yagywalkydubey08@gmail.com>
2025-02-11 01:59:38 +0530

    footer

--- React/hotelBooking/src/components/utils/ApiFunctions.jsx
+++ React/hotelBooking/src/components/utils/ApiFunctions.jsx
@@ -1,30 +1,72 @@
-import axios from "axios";
+import axios from "axios"
 
 export const api = axios.create({
-    baseURL: "http://localhost:9192",
-});
+	baseURL: "http://localhost:8080"
+})
 
-/* Add a new room to the database */
-export async function AddRoom(photo, roomType, roomPrice) {
-    const formData = new FormData();
-    formData.append("photo", photo);
-    formData.append("roomType", roomType);
-    formData.append("roomPrice", roomPrice);
+export async function addRoom(photo, roomType, roomPrice) {
+	const formData = new FormData()
+	formData.append("photo", photo)
+	formData.append("roomType", roomType)
+	formData.append("roomPrice", roomPrice)
 
-    try {
-        const response = await api.post("/rooms/add/new-room", formData);
-        return response.status === 201;
-    } catch (error) {
-        throw new Error(error.response?.data?.message || "Error adding room");
-    }
+	const response = await api.post("/rooms/add/new-room", formData)
+		// headers: getHeader()
+	
+	if (response.status === 201) {
+		return true
+	} else {
+		return false
+	}
 }
 
-/* Fetch room types from the server */
 export async function getRoomTypes() {
-    try {
-        const response = await api.get("/rooms/room-types");
-        return response.data;
-    } catch (error) {
-        throw new Error(error.response?.data?.message || "Error fetching room types");
-    }
+	try {
+		const response = await api.get("/rooms/room/types")
+		return response.data
+	} catch (error) {
+		throw new Error("Error fetching room types")
+	}
+}
+
+export async function getAllRooms() {
+	try {
+		const result = await api.get("/rooms/all-rooms")
+		return result.data
+	} catch (error) {
+		throw new Error("Error fetching rooms")
+	}
+}
+
+export async function deleteRoom(roomId) {
+	try {
+		const result = await api.delete(`/rooms/delete/room/${roomId}`, {
+			// headers: getHeader()
+		})
+		return result.data
+	} catch (error) {
+		throw new Error(`Error deleting room ${error.message}`)
+	}
+}
+
+
+export async function updateRoom(roomId, roomData) {
+	const formData = new FormData()
+	formData.append("roomType", roomData.roomType)
+	formData.append("roomPrice", roomData.roomPrice)
+	formData.append("photo", roomData.photo)
+	const response = await api.put(`/rooms/update/${roomId}`, formData,{
+		// headers: getHeader()
+	})
+	return response
+}
+
+
+export async function getRoomById(roomId) {
+	try {
+		const result = await api.get(`/rooms/room/${roomId}`)
+		return result.data
+	} catch (error) {
+		throw new Error(`Error fetching room ${error.message}`)
+	}
 }
