4c944d2c5a03db480a0b8293b8a45dc455edfe40
Anirudhdhuhan <anirudh.duhoon@gmail.com>
2025-02-11 16:00:43 +0530

    final admin commit1 frontend

---
+++ spring_boot_backend_template/spring_boot_backend_template/spring_boot_backend_template/src/main/java/com/hotelbooking/exception/UserAlreadyExistsException.java
@@ -0,0 +1,13 @@
+package com.hotelbooking.exception;
+
+public class UserAlreadyExistsException extends RuntimeException {
+
+	
+
+	public UserAlreadyExistsException(String message) {
+		// TODO Auto-generated constructor stub
+		super(message);
+	}
+
+	
+}
