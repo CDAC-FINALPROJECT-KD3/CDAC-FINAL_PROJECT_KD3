2c9499cbf4e9c00bef5647d63ede026d66f2a01b
Yagywalky <yagywalkydubey08@gmail.com>
2025-02-11 05:47:10 +0530

    pom.xml error resolved

---
+++ React/hotelBooking/src/App.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import "/node_modules/bootstrap/dist/js/bootstrap.min.js"
+import AddRoom from "./components/room/AddRoom"
+import ExistingRooms from "./components/room/ExistingRooms"
+import Home from "./components/home/Home"
+import EditRoom from "./components/room/EditRoom"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+
+import Footer from "./components/layout/Footer"
+import NavBar from "./components/layout/NavBar"
+import RoomListing from "./components/room/RoomListing"
+import Admin from './components/admin/Admin'
+
+
+function App() {
+  return( <>
+  	<main>
+				<Router>
+					<NavBar/>
+					<Routes>
+						<Route path="/" element={<Home />} />
+						<Route path="/edit-room/:roomId" element={<EditRoom />} />
+						<Route path="/existing-rooms" element={<ExistingRooms />} />
+           				<Route path="/add-room" element={<AddRoom />} />
+						   <Route path="/browse-all-rooms" element={<RoomListing />} />
+						   <Route path="/admin" element={<Admin />} />
+
+				</Routes>
+      </Router>
+	  <Footer />
+    </main>
+  </>
+  ) 
+}
+
+export default App
