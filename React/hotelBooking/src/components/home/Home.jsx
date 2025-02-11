d3fa6380403038c6d15877a238a986153470e928
Yagywalky <yagywalkydubey08@gmail.com>
2025-02-11 05:39:07 +0530

    Pom.xml error resolved

--- React/hotelBooking/src/components/home/Home.jsx
+++ React/hotelBooking/src/components/home/Home.jsx
@@ -1,12 +1,29 @@
-import React from "react"
+import React, { useContext } from "react"
+import MainHeader from "../layout/MainHeader"
+import HotelService from "../common/HotelService"
+import Parallax from "../common/Parallax"
+import RoomCarousel from "../common/RoomCarousel"
+import { useLocation } from "react-router-dom"
 
 const Home = () => {
+	const location = useLocation()
 
-    return (
-        <div>
-            <h2>Welcome to the home page</h2>
-        </div>
-    )
+	const message = location.state && location.state.message
+	const currentUser = localStorage.getItem("userId")
+	return (
+		<section>
+		
+			<MainHeader />
+			<div className="container">
+				<RoomCarousel />
+				<Parallax />
+				<RoomCarousel />
+				<HotelService />
+				<Parallax />
+				<RoomCarousel />
+			</div>
+		</section>
+	)
 }
 
 export default Home
