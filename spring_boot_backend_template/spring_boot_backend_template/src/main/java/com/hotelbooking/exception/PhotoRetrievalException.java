dfe38163478edb89e712e5de5006f5e8498b5ee4
Yagywalky <yagywalkydubey08@gmail.com>
2025-02-11 05:41:07 +0530

    pom.xml error

--- spring_boot_backend_template/spring_boot_backend_template/src/main/java/com/hotelbooking/exception/PhotoRetrievalException.java
+++
@@ -1,9 +0,0 @@
-package com.hotelbooking.exception;
-
-public class PhotoRetrievalException extends RuntimeException{
-	
-	 public PhotoRetrievalException(String message) {
-	        super(message);
-
-}
-}
